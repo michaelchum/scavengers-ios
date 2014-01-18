fb0c267004179a358b4967283ba38f53d501fab3
dcottr <davidrcottrell@gmail.com>
2014-01-18 00:33:20 -0500

    Set up call to back end

---
+++ Scavengers/LocationManager.m
@@ -0,0 +1,13 @@
+//
+//  LocationManager.m
+//  Scavengers
+//
+//  Created by David Cottrell on 1/18/2014.
+//  Copyright (c) 2014 David Cottrell. All rights reserved.
+//
+
+#import "LocationManager.h"
+
+@implementation LocationManager
+
+@end
--- Scavengers/WelcomeViewController.m
+++ Scavengers/WelcomeViewController.m
@@ -38,47 +38,31 @@
 
 - (void)loginViewFetchedUserInfo:(FBLoginView *)loginView
                             user:(id<FBGraphUser>)user {
-    _username = user.username;
-    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
-
-    NSString *path = [NSString stringWithFormat:@"http://scavengers.herokuapp.com/?username=%@", _username];
-    NSURL *url = [NSURL URLWithString:path];
-    NSURLRequest *urlRequest = [[NSURLRequest alloc] initWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:30.0];
     
-    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:urlRequest];
-    // Allowing any connections
-    operation.securityPolicy.allowInvalidCertificates = YES;
-    operation.responseSerializer = [AFJSONResponseSerializer serializer];
-    operation.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"text/html"];
-    DLog(@"Calling: %@", path);
-    DLog(@"Path: %@", path);
-    [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
-        NSDictionary *data = [responseObject objectForKey:@"data"];
-        NSArray *storyJSONs = [[data objectForKey:@"mystories"] objectForKey:@"stories"];
+    
+//    LocationTracker *locationTracker = ((AppDelegate *)[UIApplication sharedApplication].delegate).locationTracker;
+//    CLLocation *location = [locationTracker getLatestLocation];
+    
+    
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    NSMutableDictionary *parameters = [[NSMutableDictionary alloc] init];
+    
+//    [parameters setObject:[NSNumber numberWithDouble:location.coordinate.latitude] forKey:@"latitude"];
+//    [parameters setObject:[NSNumber numberWithDouble:location.coordinate.longitude] forKey:@"longitude"];
+    
+    
+    
+    [parameters setObject:_username forKey:@"username"];
+    
+    
+    
+    NSString *path = [NSString stringWithFormat:@"http://scavengers.herokuapp.com/location/"];
+    [manager POST:path parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
         
-        NSInteger userId = ((NSNumber *)[data objectForKey:@"uid"]).integerValue;
-        Story *story;
-        NSMutableArray *friendStories = [[NSMutableArray alloc] init];
-        DLog(@"storyJSONS: %@", storyJSONs);
-        for (NSDictionary *storyJSON in storyJSONs) {
-            story =[[Story alloc] initWithJSON:storyJSON];
-            story.userId = userId;
-            story.isPublic = YES;
-            
-            [friendStories addObject:story];
-        }
-        
-        // Sort by date
-        _friendStories = [NSArray arrayWithArray:friendStories];
-        NSNotification *notification = [[NSNotification alloc] initWithName:_myStoriesLoadedNotificationName object:self userInfo:nil];
-        [[NSNotificationCenter defaultCenter] postNotification:notification];
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-        DLog(@"Failed with error: %@", error);
-        // TODO
-        NSNotification *notification = [[NSNotification alloc] initWithName:_myStoriesLoadedNotificationName object:self userInfo:nil];
-        [[NSNotificationCenter defaultCenter] postNotification:notification];
+        
     }];
-    [operation start];
 }
 
 - (void)loginViewShowingLoggedOutUser:(FBLoginView *)loginView {
